bde36305bc674039371547096ca7a13a366de9e5
Sergey Zamaro <firewall606@gmail.com>
2025-06-19 15:38:45 +0300

    support for unix was added

--- src/ResourceManagementEXPORT.hpp
+++ src/ResourceManagementEXPORT.hpp
@@ -1,5 +1,15 @@
-#ifdef RESOURCE_MANAGEMENT_EXPORTS
-#define RESOURCE_MANAGEMENT_EXPORT __declspec(dllexport)
+#pragma once
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef RESOURCE_MANAGEMENT_EXPORTS
+    #define RESOURCE_MANAGEMENT_EXPORT __declspec(dllexport)
+  #else
+    #define RESOURCE_MANAGEMENT_EXPORT __declspec(dllimport)
+  #endif
 #else
-#define RESOURCE_MANAGEMENT_EXPORT __declspec(dllimport)
+  #ifdef RESOURCE_MANAGEMENT_EXPORTS
+    #define RESOURCE_MANAGEMENT_EXPORT __attribute__ ((visibility ("default")))
+  #else
+    #define RESOURCE_MANAGEMENT_EXPORT
+  #endif
 #endif
